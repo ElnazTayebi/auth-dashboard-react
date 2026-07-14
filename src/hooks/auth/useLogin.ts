8135aa2c7772b2bd71bcb6188affd8019493e7a1
ElnazTayebi <elnazt67@gmail.com>
2026-07-14 12:21:51 +0200

    fix(auth): implement token caching in useLogin onSuccess hook

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,9 +1,13 @@
-import { useMutation } from "@tanstack/react-query"
-import {login} from "@/services/auth.service"
+import { useMutation } from "@tanstack/react-query";
+import { login } from "@/services/auth.service";
 
 export function useLogin() {
-    return useMutation ({
-        mutationFn: login,
-    })
-
+  return useMutation({
+    mutationFn: login,
+    onSuccess: (res) => {
+      localStorage.setItem("token", res.accessToken);
+      localStorage.setItem("userImage", res.image);
+      localStorage.setItem("userName", `${res.firstName} ${res.lastName}`);
+    },
+  });
 }
